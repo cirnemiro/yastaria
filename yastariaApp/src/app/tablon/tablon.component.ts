4c71025dfd1673d5b0266a5c1ae798bb9cb349c0
SebasMartiar <sebas.martiar@gmail.com>
2019-04-11 13:44:58 +0200

    bootstrap y jqueri

--- yastariaApp/src/app/tablon/tablon.component.ts
+++ yastariaApp/src/app/tablon/tablon.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tablon',
+  selector: 'tablon',
   templateUrl: './tablon.component.html',
   styleUrls: ['./tablon.component.scss']
 })
 export class TablonComponent implements OnInit {
+  filtro="";
+
+  usuarios = [
+    {
+        id: 1,
+        nombre: "Taxi Ginna",
+        foto: "foto",
+        edad: 1
+    },
+    {
+        id: 2,
+        nombre: "Max Rex",
+        foto: "foto",
+        edad: 2
+    },
+    {
+        id: 3,
+        nombre: "Taxi Luna",
+        foto: "foto",
+        edad: 3
+    }
+  ]
+  
+  filtrarPorFiltro(str){
+    const subconj = this.usuarios.filter(function(aFav){
+      return (aFav.nombre.toLowerCase().indexOf(str.toLowerCase()) >= 0);
+    });
+    return subconj;
+    
+  }
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
