8b136ddec52529869d752b9b86ae4c7d221a81f3
joseprigo <jrrr13@gmail.com>
2019-04-12 16:29:57 +0200

    quienes

--- yastariaApp/src/app/app-routing.module.ts
+++ yastariaApp/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsuarioTablonComponent } from './tablon/usuario-tablon/usuario-tablon.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { TablonComponent } from './tablon/tablon.component';
+import { QuienesComponent } from './quienes/quienes.component';
 
 
 const routes: Routes = [
   { path: 'tablon', component: TablonComponent, pathMatch: 'full' },
   { path: 'usuario', component: UsuarioTablonComponent, pathMatch: 'full' },
   { path: 'registro', component: FormularioComponent, pathMatch: 'full' },
+  { path: 'quienes-somos', component: QuienesComponent, pathMatch: 'full' },
+
   { path: '', redirectTo: 'tablon', pathMatch: 'full' },
+
 
 ];
 
