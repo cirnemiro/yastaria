d9dfa9482d1105ea298b075c7fd2383ad64a9972
SebasMartiar <sebas.martiar@gmail.com>
2019-04-24 15:44:09 +0200

    error en package

--- server/app/services/jwt.js
+++ server/app/services/jwt.js
@@ -5,15 +5,18 @@ const config = require('../config/jwt');
 exports.createToken = function (user) {
     let userpayload = {
         _id: user._id,
-        name: user.name,
-        surname: user.surname,
-        email: user.email,
-        perfil: user.perfil,
-        center: user.center,
-        country: user.country,
-        city: user.city,
-        iat: moment().unix(),
-        exp: moment().add(5, 'days').unix()
+        nombre: user.nombre,
+        apellidos: user.apellidos,
+        fechaNacimiento: user.fechaNacimiento,
+        descripcion: user.descripcion,
+        foto: user.foto,
+        codigoPostal: user.codigoPostal,
+        barrio: user.barrio,
+        puntuacion: user.puntuacion,
+        ofrece: user.ofrece,
+        necesita: user.necesita,
+        correo: user.correo,
+        contra: user.contra
     };
     return jwt.create(config.TOKEN_SECRET, userpayload);
 };
